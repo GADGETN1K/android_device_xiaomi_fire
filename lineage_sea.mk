d5fb249a245d3f27f27895fc028c1fb14c1c1e5a
YgorBRxx <ygorbrxx@noreply.codeberg.org>
2026-07-01 05:46:34 -0300

    props: update product identity to ocean

--- lineage_sea.mk
+++ lineage_sea.mk
@@ -20,17 +20,17 @@ $(call inherit-product, device/xiaomi/sea/device.mk)
 PRODUCT_DEVICE := sea
 PRODUCT_NAME := lineage_sea
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := sea
+PRODUCT_MODEL := ocean
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_SYSTEM_NAME := sea_global
-PRODUCT_SYSTEM_DEVICE := sea
+PRODUCT_SYSTEM_NAME := ocean_global
+PRODUCT_SYSTEM_DEVICE := ocean
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="missi-user 15 AP3A.240905.015.A2 OS2.0.212.0.VHZMIXM release-keys" \
-    BuildFingerprint=Redmi/sea_global/sea:15/AP3A.240905.015.A2/OS2.0.212.0.VHZMIXM:user/release-keys \
+    BuildFingerprint=Redmi/ocean_global/ocean:15/AP3A.240905.015.A2/OS2.0.212.0.VHZMIXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME) \
     ProductModel=$(PRODUCT_SYSTEM_DEVICE) \
